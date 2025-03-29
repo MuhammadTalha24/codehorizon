2002dbae268c456213a44e8ab5f647fc728c517c
Muhammad Talha <talhaji880@gmail.com>
2025-03-30 02:52:09 +0500

    adding http file and code to store the user into database

---
+++ convex/http.ts
@@ -0,0 +1,57 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/nextjs/server";
+
+
+
+const http = httpRouter();
+
+http.route({
+    path: "/clerk-webhook",
+    method:"POST",
+    handler:httpAction(async(ctx,request)=>{
+        const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+        if (!webhookSecret) {
+            throw new Error("Missing CLERK_WEBHOOK_SECRET environment variable");
+        }
+
+        const svix_id = request.headers.get("svix-id");
+        const svix_signature = request.headers.get("svix-signature");
+        const svix_timestamp = request.headers.get("svix-timestamp");
+
+        if(!svix_id || !svix_signature || !svix_timestamp){
+            return new Response("Missing svix headers", { status: 400 });
+        }
+
+        const payload = await request.json();
+        const body = JSON.stringify(payload);
+        const wh = new Webhook(webhookSecret);
+        let evt: WebhookEvent;
+
+        try{
+            //Verify the webhook signature
+            evt = wh.verify(body,{
+                "svix-id":svix_id,
+                "svix-signature":svix_signature,
+                "svix-timestamp":svix_timestamp,
+            }) as WebhookEvent;
+        }catch(err){
+            //If the signature is invalid, return a 400 error
+            console.error("Error parsing webhook", err);
+            return new Response("Invalid webhook", { status: 400 });
+        }
+
+        const eventType = evt.type;
+        if(eventType === "user.created"){
+          const {id, email_addresses, first_name, last_name} = evt.data;
+          const email = email_addresses[0].email_address;
+            const name = `${first_name || ''} ${last_name || ''}`.trim();
+
+            try {
+                //Save User To Databse Convex
+            } catch (error) {
+                
+            }
+    })
+})
